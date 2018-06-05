3f7d0475982bd7975b886892b3784c3560868148
ErikBoesen <me@erikboesen.com>
2018-06-04 21:49:48 -0400

    Install xcode command line tools for git

--- SetApp.sh
+++ SetApp.sh
@@ -3,4 +3,14 @@
 apps_root=$HOME/Applications
 src_root=$HOME/src
 mkdir $apps_root $src_root
+
+# Install xcode command line tools (we need git)
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" --verbose;
+
 curl -Lo /tmp/iterm2.zip https://iterm2.com/downloads/stable/latest
